6dc5b856f6e11a82ca27805c859e7a99faf46a44
Harold Barron <haroldbarron13@gmail.com>
2022-04-28 12:49:39 -0500

    Creating the test to read a json file and save the data

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,10 @@
+const Reader = require("./../../lib/utils/Reader")
+
+describe("Testing aquisition of data", () =>
+{
+    test("1. Reading Json File", () =>
+    {
+        const Reading = Reader.readJsonFile("visualpartners.json")
+        console.log(Reading)
+    })
+})
