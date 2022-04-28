1473810aa8f06b8bbedbfdaef8ada8dbe8dd2831
Harold Barron <haroldbarron13@gmail.com>
2022-04-28 13:01:37 -0500

    Updating jest

--- test/Services/Services.test.js
+++ test/Services/Services.test.js
@@ -1,2 +1,9 @@
 const Service = require("./../../lib/Services/Services")
 
+describe("Testing Services", () =>
+{
+    test("1. Show all the students and their data", ()=>
+    {
+        
+    })
+})
