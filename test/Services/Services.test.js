ae8e25fd8a978b748a3138f0f17bb4c33bae1ea1
Harold Barron <haroldbarron13@gmail.com>
2022-04-28 13:09:07 -0500

    Finishing service to consult all the studendts in a db

--- test/Services/Services.test.js
+++ test/Services/Services.test.js
@@ -4,6 +4,9 @@ describe("Testing Services", () =>
 {
     test("1. Show all the students and their data", ()=>
     {
-        
+        const showStudents = Service.showStudents("visualpartners.json")
+        console.log(showStudents)
+        const numberStudents = showStudents.length
+        expect(numberStudents).toBe(51)
     })
 })
