d5b0aaf3ef0d8347359bf45e5ad87681687787c9
Harold Barron <haroldbarron13@gmail.com>
2022-04-28 14:19:54 -0500

    Finishing second test for the controller

--- test/Controller/ServiceController.test.js
+++ test/Controller/ServiceController.test.js
@@ -2,16 +2,17 @@ const ServiceController =require("./../../lib/Controller/ServiceController")
 
 describe("Testing the controller", () =>
 {
-    test("1.Consulting all the students and their fields", () =>
-    {
-        const allStudents = ServiceController.showStudents("visualpartners.json")
-        // const numberStudents = allStudents.length
-        // expect(numberStudents).toBe(51)
-    })
+    // test("1.Consulting all the students and their fields", () =>
+    // {
+    //     const allStudents = ServiceController.showStudents("visualpartners.json")
+    //     // const numberStudents = allStudents.length
+    //     // expect(numberStudents).toBe(51)
+    // })
     test("2. Get the email of all the students with certification", ()=>
     {
-        const studentsEmail = ServiceController.getEmails("visualpartners.json",true)
-        expect(studentsEmail.haveCertification).toBe(true)
+        const studentsEmail = ServiceController.getEmails("visualpartners.json",false)
         console.log(studentsEmail)
+        const ExplorersWithCertification = studentsEmail.forEach(certification => expect(certification.haveCertification).toBe(false));
+        
     })
 })
