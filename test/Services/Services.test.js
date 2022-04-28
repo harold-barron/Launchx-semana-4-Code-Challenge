7f2665840cdde94bd4667542861edae7d486619e
Harold Barron <haroldbarron13@gmail.com>
2022-04-28 13:14:01 -0500

    Creating the second test to get the email of all the students with certification

--- test/Services/Services.test.js
+++ test/Services/Services.test.js
@@ -5,8 +5,13 @@ describe("Testing Services", () =>
     test("1. Show all the students and their data", ()=>
     {
         const showStudents = Service.showStudents("visualpartners.json")
-        console.log(showStudents)
+        // console.log(showStudents)
         const numberStudents = showStudents.length
         expect(numberStudents).toBe(51)
+    })
+    test("2. Get the email of all the students with certification", () =>
+    {
+        const getStudentWithCertification = Service.getStudentWithCertification("visualpartners.json")
+        expect(getStudentWithCertification.haveCertification).toBe(true)
     })
 })
